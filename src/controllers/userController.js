9e8339574c02e330ff9064d4fe05ccb125495be6
emmy grammy <ojeabuo24@gmail.com>
2025-08-11 12:14:09 -0700

    src package.json

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,36 @@
 import express from 'express';
 import  User from '../models/user.js';
 
-const userController = express.Router();
-//get  all users
-router.get('/', async(req,res)=>{
+//? @route POST /api/users/register
+//? @desc Register a new user
+//? @access Public
+
+
+//post user
+export const registerUser = async (req, res) => {
     try {
-        const users = await User.find();
-        res.json(users);
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json({
+            message: 'User created successfully',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                profilePicture: user.profilePicture,
+            }
+        });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
-
-})
-
-// get one user
-router.get('/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-export default userController;
-
+};
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/connect_DB.js';
+import userRouter from './routes/userRoute.js';
+import { StatusCodes } from 'http-status-codes';
+
 
 
 const app = express();
@@ -13,9 +16,33 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 connectDB();
+//?routes
+app.use('/api/users', userRouter);
 
+
+
+//? error handling midleware
+//?404
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: '404 Not Found'
+    });
+});
+
+// ? Error handler (for thrown errors)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        success: false,
+        message: err.message || 'Internal Server Error'
+    });
+});
+
+
+//?server call
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
 
 
