84728d05661521b8bf5d12b7b23aa145435e6dc2
emmy grammy <ojeabuo24@gmail.com>
2025-08-23 13:28:04 -0700

    Update userController, user.js, userRoute.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import  User from '../models/user.js';
+import bcrypt from 'bcrypt';
+
 
 //? @route POST /api/users/register
 //? @desc Register a new user
@@ -33,4 +35,38 @@ export const registerUser = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 };
+
+//? @route POST /api/users/login
+//? @desc Login a user
+//? @access Public
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'User does not exist' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'incorrect password or email' });
+        }
+        res.status(200).json({
+            message: 'User logged in successfully',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                profilePicture: user.profilePicture,
+                token: 'token here',
+            }
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -52,8 +54,21 @@ const userSchema = new mongoose.Schema({
 },
 {
     timestamps: true
+});
+//? compare password
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
 }
-);
+
+//? Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
